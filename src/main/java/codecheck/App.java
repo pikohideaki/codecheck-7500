93f5cdcc508bc1e8dd02999d492dd4f4567ac1ce
Hideaki <noshiro.pf@gmail.com>
2017-03-17 00:30:00 +0900

    no message

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,8 +2,6 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-    	for ( int i = 0; i < args.length; ++i ) {
-        	System.out.println(args[i]);
-        }
+		System.out.println( "Hello " + args[0] );
 	}
 }
