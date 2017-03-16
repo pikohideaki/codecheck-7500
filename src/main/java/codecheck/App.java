f203d39e2009141dd02771303791bb73795624eb
code-project <dev@code-check.io>
2017-03-16 15:19:20 +0000

    Initial commit

---
+++ src/main/java/codecheck/App.java
@@ -0,0 +1,9 @@
+package codecheck;
+
+public class App {
+	public static void main(String[] args) {
+    	for ( int i = 0; i < args.length; ++i ) {
+        	System.out.println(args[i]);
+        }
+	}
+}
